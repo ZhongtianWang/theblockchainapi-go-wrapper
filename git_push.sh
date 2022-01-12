f9513a800d1204ee2b825aabf2f76cf6af1e2f1a
🎃 Zhongtian Wang <zhongtian.w@gmail.com>
2022-01-12 01:14:06 -0800

    tmp: Update mod to my personal github

--- git_push.sh
+++ git_push.sh
@@ -14,7 +14,7 @@ if [ "$git_host" = "" ]; then
 fi
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="BL0CK-X"
+    git_user_id="ZhongtianWang"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
